4db64ec8aebc73be80e640dc99f2f9aa2959e55a
Sergey Lyadzhin <lyadzhin@yandex-team.ru>
2013-11-07 19:28:16 +0400

    adding dependencies

--- src/lyadzhin/SuperTestClass1.java
+++ src/lyadzhin/SuperTestClass1.java
@@ -5,4 +5,8 @@ package lyadzhin;
  * Date: 07.11.13 18:51
  */
 public class SuperTestClass1 {
+  public static void main(String[] args) {
+    TestClassMod1 mod1 = new TestClassMod1();
+    TestClassMod2 mod2 = new TestClassMod2();
+  }
 }
